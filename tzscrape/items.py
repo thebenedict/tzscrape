db6f00b1b582289373d849d24f280df4238103e6
thebenedict <thebenedict@gmail.com>
2015-07-22 00:05:55 +0300

    update for new citizen format, add sample output

--- tzscrape/items.py
+++ tzscrape/items.py
@@ -6,6 +6,6 @@ class CitizenItem(scrapy.Item):
   publication = scrapy.Field()
   title = scrapy.Field()
   scraped_at = scrapy.Field()
-  meta = scrapy.Field()
+  byline = scrapy.Field()
   body = scrapy.Field()
   url = scrapy.Field()
